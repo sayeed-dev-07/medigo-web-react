9316886dea1127aa5b35e653cf2075f07c46114a
sayeed <expsayeedshorif@gmail.com>
2025-11-09 20:05:09 +0600

    finished the project

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,8 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { DataContext } from './Layout';
 import Card from './Card';
-import { div } from 'motion/react-client';
-
+import { motion } from 'motion/react';
 
 const Doctors = () => {
     const { data } = useContext(DataContext)
@@ -24,6 +23,20 @@ const Doctors = () => {
         let filteredArr = data.filter(item => item.name.toLowerCase().includes(lowerData))
         setArr(filteredArr)
     }
+    const parentVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.2,
+                delayChildren: 0.3
+            }
+        }
+    };
+    const childVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 }
+    };
     return (
         <div className='sm:px-[10%] font-fira sm:py-[3%] py-[8%]'>
             <div className='flex flex-col items-center justify-center gap-y-2'>
@@ -36,19 +49,19 @@ const Doctors = () => {
                 </form>
             </div>
             <div>
-                {arr.length > 0 ? 
+                {arr.length > 0 ?
                     <div className='my-[5%]'>
                         <div>
                             <p className='text-3xl sm:text-5xl font-semibold'>Doctors : </p>
                         </div>
-                        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  gap-6 py-6 '>
-                            {
-                                arr.map(item => <Card key={item.id} data={item} />)
-                            }
+                        <div  className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  gap-6 py-6 '>
+                            {arr.map((item) => (
+                                    <Card key={item.id} data={item} />
+                            ))}
                         </div>
                     </div> : <div className='flex items-center justify-center py-[15%]'>
                         <div className='text-4xl text-white '>No Doctors Found</div>
-                        </div>}
+                    </div>}
 
             </div>
         </div>
